76c64a6f33e9e3686cb228827c8cd7bc8498c096
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-10-17 00:50:37 +0700

    Update url.ts

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -27,3 +27,25 @@ export function fixUrl(url: string | URL) {
   }
   return str.replace(/([^:]\/)\/+/g, '$1')
 }
+
+export type Nullable<T> = T | null | undefined;
+
+/**
+ * transform url string to {@link Nullable}<{@link URL}>
+ * @param url
+ * @returns
+ */
+export default function toURL(url: string): Nullable<URL> {
+  try {
+    if (url.startsWith('/') || url.startsWith('?')) {
+      // url is pathname or query
+      return new URL('http://not-actually-domain.com/' + url.replace(/^\/+/, ''));
+    } else if (url.match(/^https?:\/\//)) {
+      // test full url with protocol://
+      return new URL(url);
+    }
+  } catch (error) {
+    if (error instanceof Error) console.log(url, error.message);
+    return null;
+  }
+}
