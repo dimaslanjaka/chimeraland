5bf4052f93d87d584f1aee0b3c8b3caeb9f2402a
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-28 15:34:34 +0700

    add scraper

---
+++ packages/scraper/src/chimeraland.ts
@@ -0,0 +1,21 @@
+// https://www.chimeraland.com/sea/news_detail.html?id=969b45bcaba3fa4b86ab8f8aa82aecbe4c64
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.chimeraland.com/sea/news_detail.html?id=969b45bcaba3fa4b86ab8f8aa82aecbe4c64');
+
+  const resultsSelector = '.news_container';
+
+  await page.waitForSelector(resultsSelector);
+
+  const contents = await page.evaluate((resultsSelector) => {
+    return [...document.querySelectorAll(resultsSelector)].map((anchor) => {
+      console.log(anchor.innerHTML);
+    });
+  }, resultsSelector);
+
+  await browser.close();
+})();
