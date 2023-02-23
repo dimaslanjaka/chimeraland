98a4d97c3f23dcb9838ff7003b2352a535a099f1
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-23 18:46:42 +0700

    chore: released from https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/31b5151edb4b0cbb1a9ab11f7187afec85ba4f1e

--- Recipes-list/script.js
+++ Recipes-list/script.js
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-const td = document.querySelectorAll('table#recipes td');
-Array.from(td).forEach((el) => {
-  let text = el.innerHTML;
-  text = text.replace(/\sfullness/gim, ' <img src="Recipes/fullness.jpg" class="img-inline-text" title="fullness" />');
-  text = text.replace(/\satk/gim, ' <img src="Recipes/attack.png" class="img-inline-text atk" title="attack" />');
-  text = text.replace(/\sdef/gim, ' <img src="Recipes/defense.png" class="img-inline-text def" title="defense" />');
-  el.innerHTML = text;
-});
-=======
 const e=document.querySelectorAll("table#recipes td");Array.from(e).forEach((e=>{let s=e.innerHTML;s=s.replace(/\sfullness/gim,' <img src="Recipes/fullness.jpg" class="img-inline-text" title="fullness" />'),s=s.replace(/\satk/gim,' <img src="Recipes/attack.png" class="img-inline-text atk" title="attack" />'),s=s.replace(/\sdef/gim,' <img src="Recipes/defense.png" class="img-inline-text def" title="defense" />'),e.innerHTML=s}));
->>>>>>> f80d5e979bfda4712e2d2d91a27313bc3c366ac9
