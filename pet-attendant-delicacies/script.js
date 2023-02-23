755dab3d2239cf48bdee463de55f965b04a72e6b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-24 05:49:44 +0700

    chore: released from https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/3264863d2663f80fab6323d4267a8f4b17be5ded

--- pet-attendant-delicacies/script.js
+++ pet-attendant-delicacies/script.js
@@ -1,1 +1,130 @@
-const e=document.querySelector("#a-tbl"),t=document.querySelector("#p-tbl");function getAttendant(){return fetch(attendantJson()).then((e=>e.json())).then((e=>e))}function attendantJson(){return"//backend.webmanajemen.com/chimeraland/attendant.php?show="+(new Date).getTime()}function uniq(e,t){if(!Array.isArray(e))throw new Error("array param must be instance of ARRAY");return e.filter(((n,o)=>e.findIndex((e=>n[t]===e[t]))===o))}function scrollTo(e){return new Promise((t=>{const n=e||window.location.hash.split("#")[1]||null;if(!n)return console.log("[scroll] empty id");const o="[id*='"+n+"']",a=document.querySelector(o);if(a.scrollIntoView(!0),!a)return console.log("[scroll] empty target element with id="+n);console.log("[scroll] to #"+n);"netscape"in window&&/ rv:/.test(navigator.userAgent)?a.scrollIntoView({block:"center"}):window.scroll({top:a.offsetTop,left:0,behavior:"smooth"}),t({offset:a.offsetTop,id:n,selector:o})}))}"cdpn.io"===location.host&&console.clear(),window.onload=function(){getAttendant().then((e=>(e.data=uniq(e.data,"name"),e))).then((t=>{const n=t.data;let o=[];n.forEach((e=>{let t=[];Object.keys(e).forEach((n=>{let o=e[n];"qty"===n&&(n="Quality"),"delicacies"===n&&(o=o.map((e=>{const t=e.replace("-","").toLowerCase();return`<a href='/chimeraland/Recipes.html?query=${t}'>${t}</a><span class='mr-2'>,</span>`})),o=o.join("")),Array.isArray(o)&&(o=o.join(", ")),n=n.toUpperCase(),t.push(`<tr><td>${n}</td><td>${o}</td></tr>`)}));const n=e.name.replace(/\s+/gm,"-").replace(/-+/gm,"-").toLowerCase(),a=`<div class="d-flex justify-content-between"><div id='${n}'><h3>${e.name}</h3></div><div><a href="#${n}">${n}</a></div></div>`;o.push({header:a,items:t.join("")})})),o=o.map((e=>e.header+"<table class='table'><thead><tr><th>Name</th><th>Value</th></thead><tbody>"+e.items+"</tbody></table>")),e.innerHTML=o.join(""),console.log("attendant updated successfully")})).then((()=>{scrollTo().then((e=>{setTimeout((()=>{scrollTo().then((t=>{console.log(e,t)}))}),3e3)}))}))};
+// source https://codepen.io/dimaslanjaka/pen/dymMwxL?editors=0010#shaman-regni
+// source https://www.webmanajemen.com/chimeraland/pet-attendant-delicacies.html#rosary-twinkel
+// backend https://hexo-backend.herokuapp.com/chimeraland/attendant
+// backend https://hexo-backend.herokuapp.com/chimeraland/pet
+const attendantWrapper = document.querySelector('#a-tbl');
+const petWrapper = document.querySelector('#p-tbl');
+
+if (location.host === 'cdpn.io') {
+  console.clear();
+}
+
+window.onload = function () {
+  getAttendant()
+    .then((data) => {
+      data.data = uniq(data.data, 'name');
+      //console.log(data);
+      return data;
+    })
+    .then((attendant_data) => {
+      /**
+       * @type {string[]}
+       */
+      const attendants = attendant_data.data;
+      const tbh =
+        "<table class='table'><thead><tr><th>Name</th><th>Value</th></thead><tbody>";
+      const tbf = '</tbody></table>';
+      let results = [];
+      attendants.forEach((item) => {
+        //console.log(item);
+        let build = [];
+        Object.keys(item).forEach((key) => {
+          let val = item[key];
+          if (key === 'qty') key = 'Quality';
+          if (key === 'delicacies') {
+            val = val.map((str) => {
+              const query = str.replace('-', '').toLowerCase();
+              return `<a href='/chimeraland/Recipes.html?query=${query}'>${query}</a><span class='mr-2'>,</span>`;
+            });
+            val = val.join('');
+          }
+          if (Array.isArray(val)) val = val.join(', ');
+          key = key.toUpperCase();
+          //console.log(key, val);
+          build.push(`<tr><td>${key}</td><td>${val}</td></tr>`);
+        });
+        //console.log(build);
+        const header_id = item['name']
+          .replace(/\s+/gm, '-')
+          .replace(/-+/gm, '-')
+          .toLowerCase();
+        const header = `<div class="d-flex justify-content-between"><div id='${header_id}'><h3>${item['name']}</h3></div><div><a href="#${header_id}">${header_id}</a></div></div>`;
+        results.push({ header, items: build.join('') });
+      });
+      // merge table
+      results = results.map((o) => {
+        return o.header + tbh + o.items + tbf;
+      });
+      //console.log(results);
+      attendantWrapper.innerHTML = results.join('');
+      console.log('attendant updated successfully');
+    })
+    .then(() => {
+      scrollTo().then((o) => {
+        setTimeout(() => {
+          scrollTo().then((o1) => {
+            console.log(o, o1);
+          });
+        }, 3000);
+      });
+    });
+};
+
+function getAttendant() {
+  return fetch(attendantJson())
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    });
+}
+
+function attendantJson() {
+  return (
+    '//backend.webmanajemen.com/chimeraland/attendant.php?show=' +
+    new Date().getTime()
+  );
+}
+
+// https://stackoverflow.com/a/67322087/6404439
+function uniq(arr, field) {
+  //console.log(array);
+  if (!Array.isArray(arr)) {
+    throw new Error('array param must be instance of ARRAY');
+  }
+  return arr.filter(
+    (a, i) => arr.findIndex((s) => a[field] === s[field]) === i
+  );
+}
+
+// scroll to div
+function scrollTo(id) {
+  return new Promise((resolve) => {
+    const end_id = id || window.location.hash.split('#')[1] || null;
+    if (!end_id) return console.log('[scroll] empty id');
+    const selector = "[id*='" + end_id + "']";
+    const target = document.querySelector(selector);
+    target.scrollIntoView(true);
+    if (!target)
+      return console.log('[scroll] empty target element with id=' + end_id);
+    console.log('[scroll] to #' + end_id);
+    const isFirefox = 'netscape' in window && / rv:/.test(navigator.userAgent);
+
+    if (isFirefox) {
+      // scroll to center element
+      target.scrollIntoView({ block: 'center' });
+    } else {
+      // scroll to top element
+      window.scroll({
+        top: target.offsetTop,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+
+    resolve({
+      offset: target.offsetTop,
+      id: end_id,
+      selector
+    });
+  });
+}
