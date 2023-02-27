24d6c91588aa927fe014b5793caa75a77f244a9d
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-27 23:55:48 +0700

    update scripts
    Former-commit-id: f4643a98027a79e8573462feef6734af2949585b

--- site/multisite/chimeraland/project.js
+++ site/multisite/chimeraland/project.js
@@ -4,11 +4,17 @@ const spawn = require('cross-spawn')
 const hexoProject = join(__dirname, '../../')
 const chimeralandProject = toUnix(__dirname)
 
+/**
+ * run post:copy
+ */
 function copyPost() {
   console.log('npm run copy on', hexoProject)
-  spawn('npm', ['run', 'copy'], { cwd: hexoProject, stdio: 'inherit', shell: true })
+  spawn('npm', ['run', 'post:copy'], { cwd: hexoProject, stdio: 'inherit', shell: true })
 }
 
+/**
+ * run build
+ */
 function generateSite() {
   console.log('npm run build on', hexoProject)
   spawn('npm', ['run', 'build'], { cwd: hexoProject, stdio: 'inherit', shell: true })
