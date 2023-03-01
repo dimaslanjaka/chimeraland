49ff2ea4137abab805ef772a24d47fda29f4655f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-01 22:16:23 +0700

    update chimeraland

--- site/multisite/chimeraland/src/copy-source.ts
+++ site/multisite/chimeraland/src/copy-source.ts
@@ -1,14 +1,14 @@
-import GulpClient from 'gulp'
-import { join } from 'upath'
-import { hexoProject } from '../project'
+import * as gulp from 'gulp';
+import { join } from 'upath';
+import { hexoProject } from '../project';
 
-const sourceFolder = join(__dirname, '../source')
-const sourceOutputFolder = join(hexoProject, 'source/chimeraland')
+const sourceFolder = join(__dirname, '../source');
+const sourceOutputFolder = join(hexoProject, 'source/chimeraland');
 
 export function copySource() {
-  return GulpClient.src(['**/*.*'], { cwd: sourceFolder }).pipe(
-    GulpClient.dest(sourceOutputFolder)
-  )
+  return gulp
+    .src(['**/*.*'], { base: sourceFolder })
+    .pipe(gulp.dest(sourceOutputFolder));
 }
 
-export default copySource
+export default copySource;
