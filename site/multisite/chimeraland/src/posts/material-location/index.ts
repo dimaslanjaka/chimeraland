d046a28d85aa3aeacf13d8d4b80ed8e3aa7558d1
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-07 15:47:14 +0700

    update chimeraland

--- site/multisite/chimeraland/src/posts/material-location/index.ts
+++ site/multisite/chimeraland/src/posts/material-location/index.ts
@@ -1,9 +1,36 @@
+import fm from 'front-matter'
+import fs from 'fs-extra'
+import { postMap } from 'hexo-post-parser'
+import { I18n } from 'i18n'
 import nunjucks from 'nunjucks'
 import { writefile } from 'sbg-utility'
 import slugify from 'slugify'
 import path from 'upath'
 import { chimeralandProject } from '../../../project'
 import { MaterialsData } from '../../utils/chimeraland'
+
+interface pMap extends postMap {
+  multilang?: Record<string, string>
+}
+
+const template = path.join(__dirname, 'template.md')
+const postfm = fm<pMap>(fs.readFileSync(template).toString())
+
+console.log(postfm)
+const i18n = new I18n({
+  // setup some locales - other locales default to en silently
+  locales: ['en', 'de'],
+
+  // fallback from Dutch to German and from any localized German (de-at, de-li etc.) to German
+  fallbacks: { nl: 'de', 'de-*': 'de' },
+
+  // you may alter a site wide default locale
+  defaultLocale: 'en',
+
+  // will return translation from defaultLocale in case current locale doesn't provide it
+  retryInDefaultLocale: false,
+  directory: path.join(__dirname, 'locales')
+})
 
 const data = MaterialsData.filter(
   (o) => Array.isArray(o.images) && o.images.length > 0
@@ -44,6 +71,6 @@ for (let i = 0; i < data.length; i++) {
   content += '\n\n'
 }
 
-const render = env.render(path.join(__dirname, 'template.md'), { content })
+const render = env.render(template, { content })
 const dest = path.join(chimeralandProject, 'src-posts/material-location.md')
 console.log('post written', writefile(dest, render).file)
