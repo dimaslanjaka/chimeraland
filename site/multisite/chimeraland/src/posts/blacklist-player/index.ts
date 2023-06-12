9101b2711a64d08423745e461106c4155ca7ca7f
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-06-12 14:04:40 +0700

    Update index.ts

--- site/multisite/chimeraland/src/posts/blacklist-player/index.ts
+++ site/multisite/chimeraland/src/posts/blacklist-player/index.ts
@@ -13,7 +13,7 @@ const metadata: postMeta = {
   description:
     'Blacklist player chimeraland (scammer list meliputi ruby trader, map illustrious 16 party, roll drop item, dan lain-lain). daftar SCAMMER chimeraland MAP ILLUSTRIOUS 16. daftar SCAMMER chimeraland ruby.',
   date: '2022-11-07T19:54:01+07:00',
-  updated: '2023-05-24T23:12:07+07:00',
+  updated: '2023-06-12T14:04:25+07:00',
   lang: 'id',
   permalink: '/chimeraland/blacklist-player.html',
   multilang: {
