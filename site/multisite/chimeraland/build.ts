2303f0b2122f5be9591ae1a446c91e95ab19b715
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-06 22:24:53 +0700

    chore: build using standalone functions

--- site/multisite/chimeraland/build.ts
+++ site/multisite/chimeraland/build.ts
@@ -1,6 +1,21 @@
-import './src'
-import copySource from './src/copy-source'
-import copySrcPost from './src/copy-src-posts'
+import { path } from 'sbg-utility'
+import { hexoProject } from './project'
+import * as src from './src'
 
-copySrcPost()
-copySource()
+async function main() {
+  await src.createMarkdownMaterial(
+    path.join(hexoProject, 'src-posts/chimeraland/materials')
+  )
+  await src.createMarkdownPets(hexoProject)
+  await src.createMarkdownAttendants(hexoProject)
+  await src.createMarkdownRecipe(
+    path.join(hexoProject, 'src-posts/chimeraland/recipes')
+  )
+  await src.createMarkdownScenicSpot(
+    path.join(hexoProject, 'src-posts/chimeraland/scenic-spot')
+  )
+  await src.copySrcPost(path.join(hexoProject, 'src-posts/chimeraland'))
+  await src.copySource(path.join(hexoProject, 'source/chimeraland'))
+}
+
+main()
