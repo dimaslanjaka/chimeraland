bad46b5ba93fb4d18c901f7640a6490446b1f60c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-06 22:06:16 +0700

    chore: output folder in param

--- site/multisite/chimeraland/src/copy-source.ts
+++ site/multisite/chimeraland/src/copy-source.ts
@@ -3,16 +3,16 @@ import { join } from 'upath'
 import { hexoProject } from '../project'
 
 const sourceFolder = join(__dirname, '../source')
-const sourceOutputFolder = join(hexoProject, 'source/chimeraland')
+// const sourceOutputFolder = join(hexoProject, 'source/chimeraland')
 
 /**
  * copy ../source to hexo project/source/chimeraland
  * @returns
  */
-export function copySource() {
+export function copySource(sourceOutputFolder: string) {
   return fs.copy(sourceFolder, sourceOutputFolder, { overwrite: true })
 }
 
-if (require.main === module) copySource()
+if (require.main === module) copySource(join(hexoProject, 'source/chimeraland'))
 
 export default copySource
